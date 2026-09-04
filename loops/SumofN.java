be351f7d8066616831c0e5da506139e415ca4e64
Kishore <kishoregowda170@gmail.com>
2026-09-04 10:25:36 +0530

    adding files

--- loops/SumofN.java
+++ loops/SumofN.java
@@ -6,6 +6,6 @@ public class SumofN {
         for (int i = 1; i < 10; i++) {
             sum = sum + i;
         }
-        System.out.println(sum);
+        System.out.println("Sum : "+sum);
     }
 }
