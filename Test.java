eed711ed7ed2f5d73feb78e1cd975ebb4216e8f2
Kishore <kishoregowda170@gmail.com>
2026-09-24 22:51:44 +0530

    adding files

---
+++ Test.java
@@ -0,0 +1,12 @@
+class test
+{
+	public static void main(String[] args)
+	{
+        double sum = 0;
+		for(String x : args)
+		{
+			sum = sum + Double.parseDouble(x);
+		}
+        System.out.println("Sum is " + sum);
+	}
+}
